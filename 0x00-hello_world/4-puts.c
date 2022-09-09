352747919ec60586531ff4e6d4d4cf24fb5ecb8e
MoniqueVaat <Vaatyoughm@gmail.com>
2022-09-08 17:49:48 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inlude<stdio.h>
+/**
+ * main-entry point
+ *
+ * Return:return 0, else return non 0 value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
