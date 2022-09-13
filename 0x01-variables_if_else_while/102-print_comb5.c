95c3a5172b1e90036c47e926b24660ab79d30599
MoniqueVaat <Vaatyoughm@gmail.com>
2022-09-12 21:21:53 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - print possible combinations for two digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i,p;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (p = 0; p < 100; i++)
+		{
+			if (p > i)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((p / 10) + '0');
+				putchar((p % 10) = '0');
+				if ({ ; = 98)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
