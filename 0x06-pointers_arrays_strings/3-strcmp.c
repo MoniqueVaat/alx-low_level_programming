afb4542807053936dddc64380e583779b1b94a96
MoniqueVaat <Vaatyoughm@gmail.com>
2022-09-22 15:08:46 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,23 +19,21 @@ int _strcmp(char *s1, char *s2)
 		else if (s1[i] == '\0')
 		{
 			diff = s2[i];
-
 			break;
 		}
 		else if (s2[i] == '\0')
 		{
 			diff = s1[i];
-
 			break;
 		}
 		else if (s1[i] != s2[i])
 		{
 			diff = s1[i] - s2[i];
-
 			break;
 		}
 		else
 			i++;
+
 	}
 	return (diff);
 }
