6c5cce9305de266c3ec96401dc5576b229acf6a2
MoniqueVaat <Vaatyoughm@gmail.com>
2022-10-03 02:45:24 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - add munber
+ * @argc: number of argument passed
+ * @argv: argumennt vector pointers
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0;  argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
