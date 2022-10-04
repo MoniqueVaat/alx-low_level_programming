9fe69e47105ae82601aa4b23995b03777ea9203b
MoniqueVaat <Vaatyoughm@gmail.com>
2022-10-04 11:07:06 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * len - returns lenght of str
+ * @str: string counted
+ * Return: returns the length
+ */
+
+int len(char *str)
+{
+	int len = 0;
+
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
+	return (len);
+}
