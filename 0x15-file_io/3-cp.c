a27439d1818570b8da7a45362da9aaa41d5e3ed5
MoniqueVaat <Vaatyoughm@gmail.com>
2022-11-04 13:40:19 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,6 +41,8 @@ int main(int argc, char *argv[])
 	}
 	close_src = close(src);
 	check_IO_stat(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest, NULL, 'C');
 	return (0);
 }
 
@@ -60,7 +62,7 @@ void check_IO_stat(int stat, int fd, char *filename, char mode)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d \n", fd);
 		exit(100);
 	}
-	else if (mode == 'C' && stat == -1)
+	else if (mode == 'O' && stat == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s \n", filename);
 		exit(98);
